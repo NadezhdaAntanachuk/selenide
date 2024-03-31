ff4266d664c004e80ca47de30aaec0970c17e634
NadezhdaAntanachuk <140953233+NadezhdaAntanachuk@users.noreply.github.com>
2024-03-31 21:39:32 +0300

    initial commit

---
+++ src/test/java/ru/netology/selenide/AppCardDeliveryTaskOneTest.java
@@ -0,0 +1,34 @@
+package ru.netology.selenide;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.selector.ByDeepShadow.cssSelector;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.Duration;
+
+public class AppCardDeliveryTaskOneTest {
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+    @Test
+    public void shouldBeSuccessfullyCompleted() {
+        open("http://localhost:9999");
+        $(cssSelector("[data-test-id='city'] input")).setValue("Тверь");
+        String planningDate = generateDate(4, "dd.MM.yyyy");
+        $(cssSelector("[data-test-id='date'] input")).sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $(cssSelector("[data-test-id='date'] input")).setValue(planningDate);
+        $(cssSelector("[data-test-id='name'] input")).setValue("Иван Ромашкин");
+        $(cssSelector("[data-test-id='phone'] input")).setValue("+79270000001");
+        $(cssSelector("[data-test-id='agreement']")).click();
+        $(cssSelector("button.button")).click();
+        $(cssSelector("div.notification__content"))
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(exactText("Встреча успешно забронирована на " + planningDate));
+    }
+}
